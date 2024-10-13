0125ba42e93ca0d64e83e65f457dd9551bff4589
Frikingsoft <nicolas.barone1@gmail.com>
2024-10-13 05:50:57 -0300

    respaldo

---
+++ src/components/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="menu flex flex-center">
+        <nav>
+            <router-Link :to="boton.ruta"
+            
+            v-for="(boton,index) in botones" :key="index">
+                 <q-icon :name="boton.icono" color="white" size="40px" class="q-mb-xl"/>
+            </router-Link>
+        </nav>
+    </div>
+</template>
+<script setup>
+    import { ref } from "vue";
+    const botones = ref([
+        {nombre:"Inicio",icono:"fas fa-house-user",ruta:"/"},
+        {nombre:"Login",icono:"fas fa-house-user",ruta:"/login"},
+        {nombre:"Registro",icono:"fas fa-house-user",ruta:"/registro"},
+    ])
+</script>
+<style scoped>
+   .menu{
+    width: 64px;
+    height: 100vh;
+    background-color: rgb(13, 50, 109);
+    position: fixed;
+    z-index: 1;
+    top: 0;
+   }
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <q-page class="row justify-center">
+        <q-card class="row justify-center items-center col-xs-12 col-md-6 col-xl-4 my-card formulario">
+            <q-card class="col-6 coso">
+                <q-input class="col-6" v-model="usuario" :dense="dense">
+                    <template v-slot:append>
+                        <q-avatar>
+                            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+                        </q-avatar>
+                    </template>
+                </q-input>
+            </q-card>
+        </q-card> 
+       
+    </q-page>   
+</template>
+<script setup>
+    import { ref } from "vue"
+    const usuario = ref("")
+</script>
+<style scoped>
+    .my-card{
+       box-shadow: none;
+    }
+    .coso{
+        background-color: blueviolet;
+        height: 40vh;
+    }
+</style>
